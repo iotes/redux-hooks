a1907189890b4e9d1e101d7e6a5ce4d46a7e5636
Automated Version Bump <gh-action-bump-version@users.noreply.github.com>
2020-05-16 20:28:43 +0000

    ci: version bump to v0.0.2

--- build/index.js
+++ build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@iotes/core");
 const react_1 = require("react");
-exports.createIotesReactReduxHook = (topology, strategy, reduxType = 'IOTES_HOST', busType) => {
+exports.createIotes = (topology, strategy, reduxType = 'IOTES_HOST', busType) => {
     const iotes = core_1.createIotes({ topology, strategy });
     const { hostDispatch, deviceDispatch, hostSubscribe, deviceSubscribe, } = iotes;
     const createHook = (subscribe, dispatch, selector) => {
